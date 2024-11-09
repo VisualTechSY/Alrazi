f609d4feba2e82b8054961c92daef48f7c4a0dcb
T@REK <tarek.zabibi@technolead.com>
2024-11-09 10:29:42 +0300

    fix bug

--- HttpParameters/AddTestStudentVM.cs
+++ HttpParameters/AddTestStudentVM.cs
@@ -5,7 +5,7 @@
         public int StudentId { get; set; }
         public int SerialNumber { get; set; }
         public int[] TestSubjectId { get; set; }
-        public int[] TheBase { get; set; }//القاعدي
+        public double[] TheBase { get; set; }//القاعدي
         public int[] Additional { get; set; }//الاضافي
         public double[] Mark { get; set; }
     }
