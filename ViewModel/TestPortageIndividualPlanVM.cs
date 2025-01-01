a544fe86abd12ca577c83eb8489fe3b2fbbb24b7
T@REK <DELL@DELL-PC>
2025-01-01 06:10:12 +0300

    print TestPortageIndividualPlan

---
+++ ViewModel/TestPortageIndividualPlanVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Alrazi.ViewModel
+{
+    public class TestPortageIndividualPlanVM
+    {
+        public string TitleGroup { get; set; }
+        public List<string> Quastions { get; set; }
+    }
+}
